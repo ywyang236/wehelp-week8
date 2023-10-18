bc57911574ba1e71c1407312c893c3de88f3516b
Alice <comenta0415@gmail.com>
2023-10-18 18:32:33 +0800

    Update app.py and Add single_datatype.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import *
 from api.precipitation import precipitation_app
 from api.temperature import temperature_app
 from api.location import location_app
+from api.single_datatype import single_datatype_app
 
 app = Flask(__name__)
 app.json.ensure_ascii = False
@@ -11,6 +12,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.register_blueprint(precipitation_app)
 app.register_blueprint(temperature_app)
 app.register_blueprint(location_app)
+app.register_blueprint(single_datatype_app)
 
 @app.route("/")
 def index():
