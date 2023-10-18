daa19b4ed250a34dcb908dfc6a001a4d11f15ae6
Lulu <s9341729@gmail.com>
2023-10-19 01:12:34 +0800

    Update CSS and fix click event in JS

--- static/javascripts/taiwan.js
+++ static/javascripts/taiwan.js
@@ -3,22 +3,6 @@ let mouseDiv = document.createElement("div");
 mouseDiv.className = "taiwan-area__mouse";
 taiwanMap.appendChild(mouseDiv);
 
-function handleMouseEvent(e) {
-  let path = e.target.closest("path");
-  if (path) {
-    let { left, top } = taiwanMap.getBoundingClientRect();
-    let relX = e.clientX - left;
-    let relY = e.clientY - top;
-    mouseDiv.style.left = `${relX}px`;
-    mouseDiv.style.top = `${relY}px`;
-    mouseDiv.style.display = "block";
-    mouseDiv.textContent =
-      path.getAttribute("name") || path.parentNode.getAttribute("name");
-  }
-}
-
-taiwanMap.addEventListener("click", handleMouseEvent);
-
 function handleMapClick(target) {
   let group = target.closest("g");
 
@@ -26,6 +10,15 @@ function handleMapClick(target) {
     let mapName = group
       ? group.getAttribute("name")
       : target.getAttribute("name");
+
+    let bbox = target.getBBox();
+    let relX = bbox.x;
+    let relY = bbox.y;
+
+    mouseDiv.style.left = `${relX}px`;
+    mouseDiv.style.top = `${relY}px`;
+    mouseDiv.style.display = "block";
+    mouseDiv.textContent = mapName;
 
     document.querySelectorAll(".taiwan-area__block").forEach((path) => {
       path.classList.remove("taiwan-area__block--active");
@@ -37,48 +30,9 @@ function handleMapClick(target) {
       target.classList.add("taiwan-area__block--active");
     }
 
-    // temperature
-    fetch(`/api/temperature?locationName=${mapName}`)
-      .then((response) => response.json())
-      .then((data) => {
-        // data processing
-        document.querySelector(".info__areaName").textContent =
-          data.location_name;
-        data.weather_data.forEach((item, index) => {
-          if (index === 0) {
-            document.querySelector(
-              "#info__temp .content"
-            ).textContent = `${item.minT} - ${item.maxT}`;
-            document.querySelector("#info__Rain .content").textContent =
-              item.pop;
-          } else if (index === 1) {
-            document.querySelector("#day__Period").textContent =
-              item.descriptionTime;
-            document.querySelector(
-              "#day__Temp span"
-            ).textContent = `${item.minT} - ${item.maxT}`;
-            document.querySelector("#day__Rain span").textContent = item.pop;
-          } else if (index === 2) {
-            document.querySelector("#night__Period").textContent =
-              item.descriptionTime;
-            document.querySelector(
-              "#night__Temp span"
-            ).textContent = `${item.minT} - ${item.maxT}`;
-            document.querySelector("#night__Rain span").textContent = item.pop;
-          }
-        });
-      })
-      .catch((error) => {
-        console.error("Error calling temperature:", error);
-      });
+    // call temperature
 
-    // weekly_data
-    // fetch(`/api/weekly_data?locationName=${mapName}&data_type=10`)
-    //   .then((response) => response.json())
-    //   .then((data) => console.log(data))
-    //   .catch((error) => {
-    //     console.error("Error calling weekly_data:", error);
-    //   });
+    // call weekly_data_basic
   }
 }
 
