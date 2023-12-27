c5698bb19ecf62ad2d4bcf2a65e1c4e249341474
ywyang236 <ywyang236@gmail.com>
2023-12-27 18:14:36 +0800

    Update for deploy.

--- .vercel/cache/app/static/javascripts/base.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", function () {
-    let loadingScreen = document.querySelector('.loading_screen');
-    let nav = document.querySelector('.navigation');
-    loadingScreen.style.display = "none";
-    nav.style.display = "flex";
-});
